03ddb5614a1adb572644bea7fd53c18cd1c8387d
LAN15002061 <LAN15002061@C-010065>
2021-11-12 15:57:15 +0000

    Add project files.

---
+++ Classroom2/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Classroom2.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Sender { get; set; }
+        public string Body { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+}
